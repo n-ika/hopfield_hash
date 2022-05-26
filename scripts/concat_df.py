0bae2fc5553cabe7b017af1e894603582df4bba9
n-ika <nika.jurov@gmail.com>
2022-05-26 12:59:50 -0400

    update scripts

--- scripts/concat_df.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-path="./output/results/"
-df_list=os.listdir(path)
-
-data = pd.concat([pd.read_csv(path+df) for df in df_list if df.endswith(".csv")], sort=False, ignore_index=True)
-
-data.to_csv(path+"data.csv", columns=["threshold", "N", "n", "errors", "p", "k", "type", "g", "U"], index=False)
